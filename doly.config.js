95dd475857fd03480ac4de19f3fe1cc23b3d60eb
蔡金锋 <jeff.cai@99bill.com>
2020-03-18 17:35:53 +0800

    更新默认配置

--- doly.config.js
+++ doly.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const pkg = require("./package.json");
 
 module.exports = {
   html: {
@@ -11,6 +12,9 @@ module.exports = {
     "~": path.join(__dirname, "./src")
   },
 
+  // 构建完成后生成zip包路径
+  zip: `_build/${pkg.name}-${pkg.version}.zip`,
+
   // 定义编译时变量替换
   define: {
     // api 地址
@@ -22,17 +26,27 @@ module.exports = {
   env: {
     // 生产环境
     production: {
-      publicPath: "./",
+      publicPath: "./", // 静态资源域名，如 https://img.99bill.com/
       html: {
         template: "src/document.ejs",
-        filename: "index.html",
+        filename: "seashell/webapp/product/project/index.html", // 构建后的html路径，如 seashell/webapp/x-project/agent/index.html
         minify: {
           removeComments: true,
           collapseWhitespace: true
         }
       },
+      outputFilename: "res/j/[name].[chunkhash:8].js", // js入口构建后路径
+      outputChunkFilename: "res/j/[name].[chunkhash:8].chunk.js", // js块构建后路径
+      css: {
+        filename: "res/c/[name].[contenthash:8].css", // css文件构建后路径
+        chunkFilename: "res/c/[name].[contenthash:8].chunk.css" // css块构建后路径
+      },
+      image: {
+        name: "res/i/[name].[hash:8].[ext]", // 图片构建后路径
+        limit: 1024 * 8 // 小于该大小的图片转为base64
+      },
       define: {
-        API_URL: ""
+        API_URL: "" // 生产环境的api地址
       }
     }
   }
