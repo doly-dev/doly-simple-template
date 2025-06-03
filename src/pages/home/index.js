3cd23b99c52ff1fe1df7eb306dbec37097fb024b
caijf <369756941@qq.com>
2025-06-03 10:41:08 +0800

    feat: 本地开发调试

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { sayHi } from "../../utils/util2";
+import { sum } from "../../utils/util.mjs";
 
 export default () => {
+  React.useEffect(() => {
+    sayHi();
+    console.log("sum 1 + 2 = ", sum(1, 2));
+  });
+
   return (
     <div>
       <h2>首页</h2>
